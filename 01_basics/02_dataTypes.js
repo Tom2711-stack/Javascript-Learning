13a40dd9f361105272b50ecffd8326ba62f9d944
Tomojit Bhattacharjee <bhattacharyatomojit@gmail.com>
2026-07-08 10:30:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 
+    3) // code readability should be high
+
+console.log("Tomojit")
+
+let name = "Tomojit"
+let age = 21
+let isLoggedin = true
+
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned value
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
