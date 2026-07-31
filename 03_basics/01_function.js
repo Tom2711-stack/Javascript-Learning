f703c4ccad0d4bbf7bdbfa104a05033aacabcac8
Tomojit Bhattacharjee <bhattacharyatomojit@gmail.com>
2026-07-31 14:46:21 +0000

    taking parameters in functions

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -47,5 +47,39 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
-console.log(loginUserMessage("Tomojit"))
+// console.log(loginUserMessage())
+// console.log(loginUserMessage("Tomojit"))
+
+
+function calculateCartPrice(val1, val2, ...num1){ //rest operator
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    username: "Tomojit",
+    price: 500
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+//Direct passing
+
+// handleObject({
+//     username: "Tomojit",
+//     price: 999
+// })
+
+const myNewArray = [200, 400, 600, 800]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 600, 800]))
