75c091f861adf1d1f4a37c7f063cd2284fdacc72
Tomojit Bhattacharjee <bhattacharyatomojit@gmail.com>
2026-08-07 14:45:46 +0000

    half

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// if
+// if (condition){    // if true then it will only execute
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const isUserloggedIn = true
+// const temperature = 45
+
+// if(temperature < 50){
+//     console.log("less than 50");
+// } else{
+//     console.log("temperature is greater than 50");
+// }
+
+// const temp = 45
+
+// if (temp === 45){
+//     console.log("yes")
+// }
+// else{
+//     console.log("No")
+// }
+
+
